c34bd0edb95055337910af1c60807bdcf0449fc6
Unnat Agarwal <Unnat.Agarwal@netent.com>
2017-10-05 14:35:24 +0530

    initial commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'This is a test Recipe', 'https://c1.staticflickr.com/6/5737/30622968353_35e06fcb52_b.jpg'),
+    new Recipe('Test Recipe', 'This is a test Recipe', 'https://c1.staticflickr.com/6/5737/30622968353_35e06fcb52_b.jpg')
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
