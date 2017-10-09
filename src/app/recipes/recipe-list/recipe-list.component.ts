31bfbc8456b6828f972b6c231d250d4088d710d4
Unnat Agarwal <Unnat.Agarwal@netent.com>
2017-10-09 12:39:47 +0530

    2nd set of changes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,13 +7,17 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('Test Recipe', 'This is a test Recipe', 'https://c1.staticflickr.com/6/5737/30622968353_35e06fcb52_b.jpg'),
-    new Recipe('Test Recipe', 'This is a test Recipe', 'https://c1.staticflickr.com/6/5737/30622968353_35e06fcb52_b.jpg')
+    new Recipe('Test Recipe 1', 'This is a test Recipe', 'https://c1.staticflickr.com/6/5737/30622968353_35e06fcb52_b.jpg'),
+    new Recipe('Test Recipe 2', 'This is a test Recipe', 'https://c1.staticflickr.com/6/5737/30622968353_35e06fcb52_b.jpg')
   ];
   constructor() { }
 
   ngOnInit() {
   }
-
+  onRecipeSelected(recipe: Recipe) {
+    console.log(recipe);
+    this.recipeWasSelected.emit(recipe);
+  }
 }
